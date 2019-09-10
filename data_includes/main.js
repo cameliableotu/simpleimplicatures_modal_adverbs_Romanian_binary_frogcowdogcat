2b26f913ec57550cf655b807298a1c1ea4a6c563
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-09-10 18:26:12 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -709,7 +709,7 @@ PennController("experiment" ,
 	     newImage ("frogshadowtwofrogsonstage", "frogshadowtwofrogsonstage.png")
                .print ()
 	       ,
-	       newAudio("frogsoundsshort", "frogsoundsshort.mp3")
+	       newAudio("frogsounds2sec", "frogsounds2sec.mp3")
 		.play()
 	       ,
 	       newKey(" ")
